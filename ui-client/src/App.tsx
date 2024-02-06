98374bef2593d2be079792fa94cde7f27d61d969
Fernando Falci <falci@falci.me>
2024-02-06 11:40:54 +0100

    chore(ui): base UI project

---
+++ ui-client/src/App.tsx
@@ -0,0 +1,18 @@
+import { Footer } from './components/Footer';
+import { Navbar } from './components/Navbar';
+
+function App() {
+  return (
+    <main className="dark h-screen flex flex-col text-foreground bg-background">
+      <Navbar />
+      <div className="flex flex-col justify-between items-center flex-1 p-6 max-w-5xl mx-auto w-full">
+        <div>big hero section</div>
+        <div>latest messages</div>
+        <div>form to add more</div>
+      </div>
+      <Footer />
+    </main>
+  );
+}
+
+export default App;
---
+++ ui-client/src/components/Footer.tsx
@@ -0,0 +1,7 @@
+export const Footer = () => (
+  <div className="border-t-1 border-gray-600 p-6 mt-6">
+    <p className="text-center text-sm text-gray-500">
+      © {new Date().getFullYear()} ACME Inc.
+    </p>
+  </div>
+);
