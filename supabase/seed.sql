07ef1a21f886cd73add9e4c67d67ac7d3223f9b7
Fernando Falci <falci@falci.me>
2024-12-27 15:50:27 +0100

    feat: update domain references to wholecoiners.club in seed.sql, index.html, and Home.tsx

--- supabase/seed.sql
+++ supabase/seed.sql
@@ -2,4 +2,4 @@ insert into btc.block values (828948, 0, 0);
 
 INSERT INTO settings (key, value) VALUES 
 ('edge_functions', '{"url": "https://beta.falci.me", "key": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZS1kZW1vIiwicm9sZSI6ImFub24iLCJleHAiOjE5ODM4MTI5OTZ9.CRXP1A7WOeoJeXxjNni43kdQwgnWNReilDMblYTn_I0"}'),
-('public_site', '{"domain": "btc.falci.me"}');
+('public_site', '{"domain": "wholecoiners.club"}');
--- ui-client/src/pages/Home.tsx
+++ ui-client/src/pages/Home.tsx
@@ -6,7 +6,7 @@ export const HomePage = () => (
   <div className="flex flex-col justify-around items-center h-full">
     <div>
       <h1 className="bg-clip-text tracking-tight text-transparent bg-gradient-to-r from-violet-500 via-indigo-700 to-purple-500 dark:from-violet-300 dark:via-indigo-500 dark:to-purple-300 pb-1 mb-4 text-3xl md:text-5xl pr-10">
-        Bitcoin Billboard
+        wholecoiners.club
       </h1>
     </div>
     <Messages />
